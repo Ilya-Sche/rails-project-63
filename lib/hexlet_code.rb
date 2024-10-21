97be1877d5de5ef288a64fc2aab0812acac5a337
Ilya-Sche <ilya_sche@mailfence.com>
2024-10-21 23:15:32 +0300

    Update hexlet_code.rb

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,10 +5,11 @@ require_relative "hexlet_code/version"
 module HexletCode
   def self.build(tag_name, attributes = {})
     tag_attr = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
-  if tag_attr.empty?
-    "<#{tag_name}>"
-  else
-    "<#{tag_name} #{tag_attr} />"
+    if tag_attr.empty?
+      "<#{tag_name}>"
+    else
+      "<#{tag_name} #{tag_attr} />"
+    end
   end
 end
 
