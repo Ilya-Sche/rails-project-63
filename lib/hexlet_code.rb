b0b385fd29440fba17c7409e4048a7f5ac413980
Ilya_sche <Ilya_rostov_2004@mail.ru>
2024-10-21 22:13:17 +0300

    Fixes

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,15 +4,13 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   def self.build(tag_name, attributes = {})
-      tag_attr = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
-    if tag_attr.empty?
-      "<#{tag_name}>"
-    else
-      "<#{tag_name} #{tag_attr} />"
-    end
+    tag_attr = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
+  if tag_attr.empty?
+    "<#{tag_name}>"
+  else
+    "<#{tag_name} #{tag_attr} />"
   end
 end
 
-class Error < StandardError; end
-
-end
+# class Error < StandardError; end
+# end
