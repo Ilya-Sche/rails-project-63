79f6703aa70f73031e9d7e94591eea1126dd6aec
Ilya_sche <Ilya_rostov_2004@mail.ru>
2024-10-29 22:51:51 +0300

    Добавил текстовые поля и метод input

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
+require 'active_support/core_ext/string'
 # HexletCode - это модуль, который предоставляет...
 module HexletCode
   def self.build(tag_name, attributes = {})
@@ -12,12 +13,83 @@ module HexletCode
     end
   end
 
-  def self.form_for(attr = {}, &block)
-    user_attr = attr.map { |key, value| "#{key}='#{value}'" }.join(' ')
-    result = "<form#{user_attr.empty? ? '' : " #{user_attr}"}>"
-    (block_given? && block.call) || ''
-    result += yield
-    result += '</form>'
-    result
+  def self.form_for(user, action: "#", method: "post", url: nil)
+    result = []
+    form = UserForm.new(user)
+
+    if url
+      result << "<form action='#{url}' method='#{method}'>"
+    else
+      result << "<form action='#{action}' method='#{method}'>"
+    end
+
+    result << yield(form) if block_given?
+    result << "</form>"
+    result.join('')
+  end
+  
+
+  require 'ostruct'
+  class UserForm
+
+    def initialize(user)
+      @user = user
+      @array = []
+    end
+  
+    def input(input_name, **options)
+      value = @user.public_send(input_name) || ''    
+      type = options.fetch(:as, 'text')
+      additional_attributes = options.reject { |key| key == :as }
+
+      if additional_attributes.any?
+        attr_string = additional_attributes.map { |key, value| "#{key}='#{value}'" }.join(' ')
+        @array << attr_string if attr_string&.empty?
+      end
+    
+      if type == :text
+        cols = options.fetch(:cols, '10')
+        rows = options.fetch(:rows, '20')
+        @array << "<textarea name='#{input_name}' cols='#{cols}' rows='#{rows}'>#{value}</textarea>"
+      else
+        attributes = [
+          "name='#{input_name}'",
+          "type='#{type}'",
+          "value='#{value}'",
+          (attr_string unless attr_string.nil?)
+        ].compact.join(' ')
+
+        @array << "<input #{attributes}>"
+      end
+    end
   end
 end
+
+# "<form action='#' method='post'><input name='name' type='text' value='rob'><textarea name='job' cols='20' rows='40'>hexlet</textarea></form>"
+# result = HexletCode.form_for(user) do |f|
+#   f.input :name
+#   f.input :job, as: :text
+# end
+
+
+
+# def self.form_for(user, method: "#", action: "post")
+#   result = []
+#   form = UserForm.new(user)
+#   result << "<form action='#{action}' method='#{method}'>"
+#   result << = yield(form) if block_given?
+#   result << = "</form>"
+#   result
+# end
+
+# def self.form_for(user, attr = {}, &block)
+# form = UserForm.new(user)
+#   action = attr.delete(:url) || '#'
+#   user_attr = attr.map { |key, value| "#{key}='#{value}'" }.join(' ')
+#   result = "<form#{user_attr.empty? ? '' : " #{user_attr}"}>"
+#   yield(form) if block_given?
+#   result << form.to_s
+#   result << "</form>"
+#   result
+# end
+# type = options.fetch(:as, 'text')
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -2,23 +2,54 @@
 
 require 'minitest/autorun'
 require_relative '../lib/hexlet_code'
-
+require 'ostruct'
+# HexletCodeTest - это модуль, который предоставляет...
 class HexletCodeTest < Minitest::Test
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
   def test_form_for_without_attributes
-    user = Struct.new(:name, :job, keyword_init: true).new(name: 'rob', job: 'developer')
+    user = User.new(name: 'rob', job: 'developer')
     result = HexletCode.form_for(user)
-    assert_equal '<form></form>', result
+    assert_equal "<form action='#' method='post'></form>", result
   end
 
   def test_form_for_with_attributes
-    user = Struct.new(:name, :job, keyword_init: true).new(name: 'rob', job: 'developer')
+    user = User.new(name: 'rob', job: 'developer')
     result = HexletCode.form_for(user, action: '#', method: 'post')
     assert_equal "<form action='#' method='post'></form>", result
   end
 
   def test_form_for_with_block_content
-    user = Struct.new(:name, :job, keyword_init: true).new(name: 'rob', job: 'developer')
-    result = HexletCode.form_for(user, action: '/users', method: 'post')
+    user = User.new(name: 'rob', job: 'developer')
+    result = HexletCode.form_for(user, url: '/users', method: 'post')
     assert_equal "<form action='/users' method='post'></form>", result
+  end
+
+  def test_form_for_input_name_job_as_text
+    user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
+    result = HexletCode.form_for(user) do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+    assert_equal "<form action='#' method='post'><input name='name' type='text' value='rob'>" \
+                 "<textarea name='job' cols='10' rows='20'>hexlet</textarea></form>", result
+  end
+
+  def test_form_for_input_name_additional_attribute
+    user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
+    result = HexletCode.form_for(user, url: '#') do |f|
+      f.input :name, class: 'user-input'
+      f.input :job
+    end
+    assert_equal "<form action='#' method='post'><input name='name' type='text' value='rob' class='user-input'>" \
+                 "<input name='job' type='text' value='hexlet'></form>", result
+  end
+
+  def test_change_cols_and_rows
+    user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
+    result = HexletCode.form_for(user, url: '#') do |f|
+      f.input :job, as: :text, rows: 50, cols: 50
+    end
+    assert_equal "<form action='#' method='post'>" \
+                 "<textarea name='job' cols='50' rows='50'>hexlet</textarea></form>", result
   end
 end
